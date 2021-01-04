9c5cd1501b265567f781ec1c1cf41d548c47ac2c
jarbasal <jarbasai@mailfence.com>
2021-01-04 23:11:48 +0000

    ovos

--- __init__.py
+++ __init__.py
@@ -1,60 +1,52 @@
-from mycroft.skills.common_play_skill import CommonPlaySkill, \
-    CPSMatchLevel, CPSMatchType
-import youtube_dl
+from ovos_utils.skills.templates.media_player import MediaSkill, \
+    CPSMatchType, CPSMatchLevel, CPSTrackStatus
 from os.path import join, dirname
+import pyvod
 
 
-class OldWorldRadioSkill(CommonPlaySkill):
+class OldWorldRadioSkill(MediaSkill):
     def __init__(self):
         super().__init__()
+        self.settings["audio_only"] = True
         self.supported_media = [CPSMatchType.RADIO]
-
-    def initialize(self):
-        self.add_event('skill-old-world-radio.jarbasskills.home',
-                       self.handle_homescreen)
+        self.default_bg = join(dirname(__file__), "ui", "logo.png")
+        self.default_image = join(dirname(__file__), "ui", "background.jpg")
+        self.message_namespace = 'skill-old-world-radio.jarbasskills.home'
+        self.bootstrap_list = ["https://www.youtube.com/watch?v=tzBGEqkwCoY"]
 
     def get_intro_message(self):
         self.speak_dialog("intro")
         self.gui.show_image(join(dirname(__file__), "ui", "logo.jpg"))
 
-    # homescreen
-    def handle_homescreen(self, message):
-        # TODO selection menu
-        self.CPS_play("https://www.youtube.com/watch?v=tzBGEqkwCoY",
-                      utterance="vlc")
-
     def CPS_match_query_phrase(self, phrase, media_type):
         match = CPSMatchLevel.GENERIC
         url = "https://www.youtube.com/watch?v=tzBGEqkwCoY"
+        image = join(dirname(__file__), "ui", "background.jpg")
 
         if self.voc_match(phrase, "old_world"):
             match = CPSMatchLevel.EXACT
         elif self.voc_match(phrase, "vintage"):
             url = "https://www.youtube.com/watch?v=tb0B3auGbtA"
             match = CPSMatchLevel.TITLE
+            image = join(dirname(__file__), "ui", "background2.jpg")
 
-        return (phrase, match, {"url": url})
+        if match is not None:
+            return (phrase, match,
+                    {"media_type": media_type, "query": phrase,
+                     "image": image, "background": self.default_bg,
+                     "stream": url})
+        return None
 
     def CPS_start(self, phrase, data):
-        ydl_opts = {
-            'format': "91",
-            "no_color": True
-        }
-
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            result = ydl.extract_info(data["url"],
-                                      download=False
-                                      # We just want to extract the info
-                                      )
-
-            if 'entries' in result:
-                # Can be a playlist or a list of videos
-                video = result['entries'][0]
-            else:
-                # Just a video
-                video = result
-
-        self.CPS_play(video['url'], utterance="vlc")
+        bg = data.get("background") or self.default_bg
+        image = data.get("image") or self.default_image
+        url = pyvod.utils.get_video_stream(data["stream"])
+        self.CPS_send_status(uri=url,
+                             image=image,
+                             background_image=bg,
+                             playlist_position=0,
+                             status=CPSTrackStatus.PLAYING_AUDIOSERVICE)
+        self.audioservice.play(url, utterance=self.play_service_string)
 
 
 def create_skill():
